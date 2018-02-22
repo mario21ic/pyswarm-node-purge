b70d2dd2c9fe72018e473d579e06ff9277f011f9
Mario Inga <mario@yarkan.pe>
2018-02-22 10:35:56 -0500

    fix(deploy) adding aws region

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #1/bin/bash
 
-docker run -v $PWD/.aws:/root/.aws -v /var/run/docker.sock:/var/run/docker.sock mario21ic/swarm-ec2-purge python main.py
+docker run -e AWS_REGION=$AWS_REGION -v $PWD/.aws:/root/.aws -v /var/run/docker.sock:/var/run/docker.sock mario21ic/swarm-ec2-purge python main.py
